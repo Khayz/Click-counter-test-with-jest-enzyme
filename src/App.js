e8e60fd8b32737f4634a7b33764568749ecf2c0f
Carlos Robles <altaircarn@gmail.com>
2020-08-19 15:43:14 -0500

    App and test withit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
-import "./App.css";
 
 export default class App extends Component {
+  state = {
+    counter: 0,
+    isZero: false,
+  };
+
+  incrementCounter = () => {
+    console.log(this.state.isZero);
+    this.setState({
+      counter: this.state.counter + 1,
+      isZero: false,
+    });
+  };
+
+  decrementCounter = () => {
+    this.setState({
+      counter:
+        this.state.counter > 0 ? this.state.counter - 1 : this.state.counter,
+      isZero: this.state.counter === 0 && true,
+    });
+  };
+
   render() {
     return (
       <div data-test="component-app">
-        <h1>App</h1>
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        <button onClick={this.incrementCounter} data-test="increment-button">
+          Increment counter
+        </button>
+        <button onClick={this.decrementCounter} data-test="decrement-button">
+          Decrement counter
+        </button>
+        {this.state.isZero && (
+          <p data-test="error-message">The counter cant go below zero</p>
+        )}
       </div>
     );
   }
